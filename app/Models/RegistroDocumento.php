63fdda0fa29d7608293fb82817f2861ee7ecd805
Pedro Henrique <pedrohenriqueribei@gmail.com>
2025-11-16 17:38:21 -0300

    Ajuste no nome do atributo de relacionamento com prontuário psicológico e adição dos métodos de formatação de data

--- app/Models/RegistroDocumento.php
+++ app/Models/RegistroDocumento.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ class RegistroDocumento extends Model
     use HasFactory;
     protected $table = 'registros_documentos';
     protected $fillable = [
-        'prontuario_psi_id',
+        'prontuario_psicologico_id',
         'finalidade',
         'destinatario',
         'data_registro',
@@ -20,6 +21,27 @@ class RegistroDocumento extends Model
     
     public function prontuario()
     {
-        return $this->belongsTo(ProntuarioPsicologico::class, 'prontuario_psi_id');
+        return $this->belongsTo(ProntuarioPsicologico::class, 'prontuario_psicologico_id');
+    }
+
+    public function getDataCriacaoAttribute() {
+        return Carbon::parse($this->created_at)->format('d/m/Y');
+    }
+
+    //data de criação completo
+    public function getDataCriacaoCompletaAttribute()
+    {
+        return Carbon::parse($this->created_at)->format('d \d\e F \d\e Y \à\s H:i');
+    }
+
+    //data de atualização formatado
+    public function getDataAtualizacaoAttribute() {
+        return Carbon::parse($this->updated_at)->format('d/m/Y');
+    }
+
+    //data atualização completa
+    public function getDataAtualizacaoCompletaAttribute()
+    {
+        return Carbon::parse($this->updated_at)->format('d \d\e F \d\e Y \à\s H:i');
     }
 }
--- app/Models/RegistroEncaminhamento.php
+++ app/Models/RegistroEncaminhamento.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +12,7 @@ class RegistroEncaminhamento extends Model
     use HasFactory;
     protected $table = 'registros_encaminhamento';
     protected $fillable = [
-        'prontuario_psi_id',
+        'prontuario_psicologico_id',
         'descricao',
         'tipo',
         'data_registro',
@@ -19,6 +20,27 @@ class RegistroEncaminhamento extends Model
     
     public function prontuario()
     {
-        return $this->belongsTo(ProntuarioPsicologico::class, 'prontuario_psi_id');
+        return $this->belongsTo(ProntuarioPsicologico::class, 'prontuario_psicologico_id');
+    }
+
+    public function getDataCriacaoAttribute() {
+        return Carbon::parse($this->created_at)->format('d/m/Y');
+    }
+
+    //data de criação completo
+    public function getDataCriacaoCompletaAttribute()
+    {
+        return Carbon::parse($this->created_at)->format('d \d\e F \d\e Y \à\s H:i');
+    }
+
+    //data de atualização formatado
+    public function getDataAtualizacaoAttribute() {
+        return Carbon::parse($this->updated_at)->format('d/m/Y');
+    }
+
+    //data atualização completa
+    public function getDataAtualizacaoCompletaAttribute()
+    {
+        return Carbon::parse($this->updated_at)->format('d \d\e F \d\e Y \à\s H:i');
     }
 }
